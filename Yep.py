fb022ec406eb9cfac978df7870b85c6a9ce42c36
gBIGJOHN <greyson.littlejohn2013@gmail.com>
2025-05-08 20:23:16 -0400

    Add files via upload

---
+++ Yep.py
@@ -0,0 +1,19 @@
+print('Unemployment Quiz, if you pass you are unemployed: Difficulty Medium')
+
+print('Question 1: Is "What the sigma" Brainrot')
+if input() == 'Yes':
+    print('Correct')
+else:
+    print('This is Brairot, not whatever you said')
+
+print('Question 2: Whats a word that starts with J and ends with B and is a 3 letter word')
+if input() == 'J*b':
+    print('Correct')
+else:
+    print('The correct answer is J*b not Job because that is a slur')
+
+print('Question 3: Have you watched every Skibidi Toilet episode?')
+if input() == 'Yes':
+    print('Good Boy')
+else:
+    print('You dissapoint me!!!!')
